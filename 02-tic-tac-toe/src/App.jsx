562011acab4e44098895d9b5e561d837e56d3631
lauracolladoq <laucolqui@gmail.com>
2024-08-02 09:11:25 +0200

    Add localStorage

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -4,11 +4,22 @@ import { Square } from './components/Square.jsx'
 import { TURNS } from './constants'
 import { checkWinner, checkEndGame } from './logic/board'
 import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameStorage, resetGameStorage } from './logic/storage/index.js'
 
 function App() {
   // Cada vez que se haga click en el tablero hay que renderizarlo
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    // Si hay algo en el local storage lo parseo, si no, creo un array de 9 posiciones con null
+    return boardFromStorage ? JSON.parse(boardFromStorage) :
+      Array(9).fill(null)
+  })
+  
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
   // Null no hay ganador y false es empate
   const [winner, setWinner] = useState(null)
 
@@ -23,6 +34,8 @@ function App() {
     // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Guardar partida
+    saveGameStorage({ board: newBoard, turn: newTurn })
     // Revisar si hay ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
@@ -38,6 +51,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   return (
