ba20b486a3e67a42c3b1851f8a8e7c6c24a170b7
lauracolladoq <laucolqui@gmail.com>
2024-08-05 13:57:51 +0200

    Add pointer tracking and enable/disable feature

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,6 +1,56 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('useEffect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // Se ejecuta cuando se desmonta el componente (deja de renderizarse)
+    return () => {
+      // Limpieza de la suscripción
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Disable' : 'Enable'} pointer tracking
+      </button>
+    </>
+  )
+}
+
 function App() {
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
