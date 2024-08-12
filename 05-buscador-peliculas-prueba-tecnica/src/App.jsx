db4720694f50360620939a83b142fbd3bb5e12d7
lauracolladoq <laucolqui@gmail.com>
2024-08-12 13:25:46 +0200

    Refactor file structure

--- 05-buscador-peliculas-prueba-tecnica/src/App.jsx
+++ 05-buscador-peliculas-prueba-tecnica/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import { Movies } from './components/Movies'
 
 function App() {
+  const movies = responseMovies.Search
+  console.log(responseMovies)
   return (
-    <>
-    </>
+    <div>
+      <header>
+        <h1>Movie finder</h1>
+        <form action="" className='form'>
+          <input type="text" placeholder='Avengers, Star Wars, The Matrix...' />
+          <button type="submit">Search</button>
+        </form>
+      </header>
+      <main>
+        <Movies movies={movies} />
+      </main>
+    </div>
   )
 }
 
